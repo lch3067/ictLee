773c4d9cb1cb02204669aa1b3cdc0d906d7c4421
lch3067 <lch3067@naver.com>
2025-07-10 10:40:58 +0900

    [백앤드] - wineDto.java 생성 : 와인데이터를 넣기 위해서 객체Dto를 만들었습니다.

---
+++ src/main/dto/wineDto.java
@@ -0,0 +1,56 @@
+package main.dto;
+
+// 와인 객체
+public class wineDto {
+
+	// 와인 이름
+	private String wine_name;
+	// 와인 가격
+	private int wine_price;
+	// 와인 산지
+	private String wine_growing;
+	// 도수
+	private String wine_strength;
+	
+	// 기본 생성자는 매개변수가 없는 경우 생성하는 목적이지만, 
+	// 굳히 setter와 getter를 사용하여 맴버변수이 넣을거면, 생성자로 넣지 않아도 된다.
+	// 자동으로 생성해주기 때문이다.
+	public wineDto() {}
+	
+	// 하지만, 매개변수가 있는 생성자를 만들면 기본 생성자도 같이 만들어 줘야한다.
+	public wineDto(String wine_name, int wine_price, String wine_growing, String wine_strength) {
+		super();
+		this.wine_name = wine_name;
+		this.wine_price = wine_price;
+		this.wine_growing = wine_growing;
+		this.wine_strength = wine_strength;
+	}
+
+	// getter, setter : 맴버변수 값을 반환 및 입력을 위한 목적
+	public String getWine_name() {
+		return wine_name;
+	}
+	public void setWine_name(String wine_name) {
+		this.wine_name = wine_name;
+	}
+	public int getWine_price() {
+		return wine_price;
+	}
+	public void setWine_price(int wine_price) {
+		this.wine_price = wine_price;
+	}
+	public String getWine_growing() {
+		return wine_growing;
+	}
+	public void setWine_growing(String wine_growing) {
+		this.wine_growing = wine_growing;
+	}
+	public String getWine_strength() {
+		return wine_strength;
+	}
+	public void setWine_strength(String wine_strength) {
+		this.wine_strength = wine_strength;
+	}
+	
+	
+}
